e90488bbcb78071211e0ed75275866ef06481737
Mooncake Sugar <60507815+Tsukina-7mochi@users.noreply.github.com>
2024-03-30 18:48:24 +0900

    feat: implement udon magazine

---
+++ Packages/net.ts7m.udon-magazine/Script/Udon/UdonMagazineHandle.cs
@@ -0,0 +1,38 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Utility;
+
+namespace net.ts7m.udon_magazine.script.udon
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class UdonMagazineHandle : UdonSharpBehaviour
+    {
+        [SerializeField] private UdonMagazine magazine;
+        [SerializeField] private bool backward;
+
+        public void Start()
+        {
+            if (magazine == null)
+            {
+                Debug.LogError("[UdonMagazineHandle] Magazine is not set.");
+            }
+        }
+
+        public override void Interact()
+        {
+            if (magazine == null) return;
+            
+            if (backward)
+            {
+                magazine.Backward();
+            }
+            else
+            {
+                magazine.Forward();
+            }
+        }
+    }
+}

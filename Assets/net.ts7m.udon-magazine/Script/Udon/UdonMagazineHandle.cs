440192745ba2a0f05cb57bbf68a3e30505668a15
Mooncake Sugar <60507815+Tsukina-7mochi@users.noreply.github.com>
2024-07-31 01:19:15 +0900

    style: reformat code

--- Assets/net.ts7m.udon-magazine/Script/Udon/UdonMagazineHandle.cs
+++ Assets/net.ts7m.udon-magazine/Script/Udon/UdonMagazineHandle.cs
@@ -1,36 +1,25 @@
-﻿
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
-using VRC.SDKBase;
-using VRC.Udon;
-using VRC.Utility;
 
-namespace net.ts7m.udon_magazine.script.udon
-{
+namespace net.ts7m.udon_magazine.script.udon {
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
-    public class UdonMagazineHandle : UdonSharpBehaviour
-    {
+    public class UdonMagazineHandle : UdonSharpBehaviour {
         [SerializeField] private UdonMagazine magazine;
         [SerializeField] private bool backward;
 
-        public void Start()
-        {
-            if (magazine == null)
-            {
+        public void Start() {
+            if (magazine == null) {
                 Debug.LogError("[UdonMagazineHandle] Magazine is not set.");
             }
         }
 
-        public override void Interact()
-        {
+        public override void Interact() {
             if (magazine == null) return;
-            
-            if (backward)
-            {
+
+            if (backward) {
                 magazine.Backward();
             }
-            else
-            {
+            else {
                 magazine.Forward();
             }
         }
